410fd4c183b9c95b8b1b78f0c5020c03a6629859
Niall Burdon <n.burdon@protonmail.com>
2022-05-12 11:56:40 +0200

    CSS

---
+++ src/Sidebar.js
@@ -0,0 +1,24 @@
+
+const Sidebar = () => {
+    return(
+        <div className="app-sidebar">
+            <div className= "app-sidebar-header">
+                <h1>Notes</h1>
+                <button>Add</button>
+            </div>
+
+            <div className="app-sidebar-notes">
+                <div className="app-sidebar-note">
+                    <div className="sidebar-note-title">
+                        <strong>TITLE</strong>
+                        <button>Delete</button>
+                    </div>
+                    <p>Notes preview</p>
+                    <small className="note-meta">Last modified [date]</small>
+                </div>
+            </div>
+        </div>
+    ); 
+};
+
+export default Sidebar;
